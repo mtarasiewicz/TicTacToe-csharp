c3e4c588900bf111832330d0583c14828ebd1ab9
Marcin Tarasiewicz <marcin13101999@gmail.com>
2023-03-26 20:26:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe game = new TicTacToe();
+            var round = 0;
+            var player = 0;
+            var choose = 0;
+
+            do
+            {  
+                Console.WriteLine("Gracz nr 1: X, Gracz nr 2: 0");
+                game.Board();
+                if (game.IsDraw())
+                {
+                    break; 
+                }
+
+                player = (round % 2) + 1;
+                Console.WriteLine("Tura gracza {0}", player);
+                bool validator = false;
+
+                while (!validator)
+                {
+                    Console.Write("Wybierz pozycje (1 - 9): ");
+                    choose = int.Parse(Console.ReadLine());
+
+                    if(choose > 0 && choose < 10)
+                    {
+                        if (game.IsDuplicate(choose))
+                        {
+                            Console.Beep();
+                            Console.WriteLine("Pole jest juz zajęte!");
+                        }else
+                        {                           
+                            game.Move(round%2, choose);
+                            validator = true;
+                        }   
+                    }else
+                    {
+                        Console.Beep();
+                        Console.WriteLine("Wprowadź prawidłową wartość!");
+                    }
+                }
+
+                Console.Clear();
+                round++;
+
+            } while (!game.IsWin());
+            
+            
+            Console.Clear();
+            if(game.IsDraw())
+            {
+                Console.WriteLine("REMIS!");
+                game.Board();
+                Console.ReadKey();
+            }else
+            {
+                Console.WriteLine("Wygrał gracz nr {0}!", player);
+                game.Board();
+                Console.ReadKey();
+            }
+           
+        }
+    }
+}
---
+++ TicTacToe.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class TicTacToe
+    {
+        char[] board = {'1', '2', '3' ,'4', '5', '6', '7', '8', '9'};
+        public TicTacToe() { }
+        public void Board()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[0], board[1], board[2]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[3], board[4], board[5]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[6], board[7], board[8]);
+            Console.WriteLine("     |     |      ");
+        }
+        public void Move(int player, int coordinate)
+        {
+            if (player == 0)
+            {
+                board[coordinate - 1] = 'X';
+            }else if(player == 1)
+            {
+                board[coordinate - 1] = 'O';
+            }
+        }
+        public bool IsWin()
+        {
+            if (board[0] == board[1] && board[1] == board[2] ||
+                board[3] == board[4] && board[4] == board[5] ||
+                board[6] == board[7] && board[7] == board[8] ||
+                board[0] == board[3] && board[3] == board[6] ||
+                board[1] == board[4] && board[4] == board[7] ||
+                board[2] == board[5] && board[5] == board[8] ||
+                board[0] == board[4] && board[4] == board[8] ||
+                board[2] == board[4] && board[4] == board[6])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            } 
+
+        }
+        public bool IsDraw()
+        {
+            if (board[0] != '1' &&
+                board[1] != '2' &&
+                board[2] != '3' &&
+                board[3] != '4' &&
+                board[4] != '5' &&
+                board[5] != '6' &&
+                board[6] != '7' &&
+                board[7] != '8' &&
+                board[8] != '9')
+            {
+                return true;
+            }
+            else return false;
+        }
+        public bool IsDuplicate(int coordinate)
+        {
+            if (board[coordinate - 1].Equals('X') || board[coordinate - 1].Equals('O'))
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
